7b5600c227256d47423121b8fc6bfd20a643db73
Gabbie <gabbie.metheny@gmail.com>
2018-10-06 06:18:52 -0700

    sync my files to Guil's starting files

---
+++ src/components/App.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: 'Guil',
+        id: 1
+      },
+      {
+        name: 'Treasure',
+        id: 2
+      },
+      {
+        name: 'Ashley',
+        id: 3
+      },
+      {
+        name: 'James',
+        id: 4
+      }
+    ]
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState((prevState) => {
+      return {
+        players: prevState.players.filter((player) => {
+          return player.id !== id;
+        })
+      };
+    });
+  };
+
+  render() {
+    return(
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length}
+        />
+  
+        {/* Players list */}
+        {this.state.players.map((player) =>
+          <Player
+            key={player.id.toString()}
+            id={player.id}
+            name={player.name}
+            removePlayer={this.handleRemovePlayer}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="stats">
+        Players: {props.totalPlayers}
+      </span>
+    </header>
+  );
+};
+
+const Player = (props) => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        <button
+          className="remove-player"
+          onClick={() => props.removePlayer(props.id)}
+        >
+          ✖
+        </button>
+        {props.name}
+      </span>
+
+      <Counter />
+    </div>
+  );
+};
+
+class Counter extends Component {
+  state = {
+    score: 0
+  };
+
+  incrementScore = () => {
+    this.setState((prevState) => {
+      return {
+        score: prevState.score + 1
+      }
+    });
+  };
+
+  decrementScore = () => {
+    this.setState((prevState) => {
+      return {
+        score: prevState.score - 1
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
+}
+
+export default App;
