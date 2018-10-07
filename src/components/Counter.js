1d51b0c2602ad015baa815e054f163359fe4f57f
Gabbie <gabbie.metheny@gmail.com>
2018-10-07 06:00:34 -0700

    convert stateless class component to functional component, comment out broken methods

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
+const Counter = (props) => {
 
-  incrementScore = () => {
-    this.setState((prevState) => {
-      return {
-        score: prevState.score + 1
-      }
-    });
-  };
+  // incrementScore = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       score: prevState.score + 1
+  //     }
+  //   });
+  // };
 
-  decrementScore = () => {
-    this.setState((prevState) => {
-      return {
-        score: prevState.score - 1
-      }
-    });
-  };
+  // decrementScore = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       score: prevState.score - 1
+  //     }
+  //   });
+  // };
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.props.score}</span>
-        <button className="counter-action increment"> + </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">{props.score}</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
 
 export default Counter;
