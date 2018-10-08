a6aec0831ab3b7159044ad3051905c3c465d711a
Gabbie <gabbie.metheny@gmail.com>
2018-10-08 06:13:21 -0700

    update Stats with total points

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Stats = (props) => {
+  const totalPlayers = props.players.length;
+  const totalPoints = props.players.reduce((total, player) => {
+    return total + player.score;
+  }, 0);
+
   return (
     <table className="stats">
       <tbody>
         <tr>
           <td>Players:</td>
-          <td>{props.players.length}</td>
+          <td>{totalPlayers}</td>
         </tr>
         <tr>
           <td>Total Points:</td>
-          <td>0</td>
+          <td>{totalPoints}</td>
         </tr>
       </tbody>
     </table>
