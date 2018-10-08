97dd4b3216b43bb1b3bb38e5181dfa43b3d98a22
Gabbie <gabbie.metheny@gmail.com>
2018-10-08 05:51:09 -0700

    add Stats component to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Stats from './Stats';
 
 const Header = (props) => {
   return (
     <header>
+      <Stats />
       <h1>{props.title}</h1>
-      <span className="stats">
-        Players: {props.totalPlayers}
-        </span>
     </header>
   );
 };
