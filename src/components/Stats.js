df38c9401de45f3873d811257dfdf112711c2f66
Gabbie <gabbie.metheny@gmail.com>
2018-10-08 05:50:41 -0700

    initial commit

---
+++ src/components/Stats.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Stats = (props) => {
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>0</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Stats;
