1d7424ce2ca5d36505bb9b63d5f640dcaa612db6
Gabbie <gabbie.metheny@gmail.com>
2018-10-09 05:40:43 -0700

    make form a controlled component

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
 
 class AddPlayerForm extends Component {
+  state = {
+    value: ''
+  };
+
+  handleValueChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
   render() {
     return (
       <form>
         <input
           type="text"
+          value={this.state.value}
+          onChange={this.handleValueChange}
           placeholder="Enter a player's name"
         />
         <input
