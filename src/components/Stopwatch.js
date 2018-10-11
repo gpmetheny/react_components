d30656acf1bef95b2c54b1d0cc45add0b29f75cf
Gabbie <gabbie.metheny@gmail.com>
2018-10-11 05:01:18 -0700

    add click handler to start/stop button

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -5,12 +5,18 @@ class Stopwatch extends Component {
     isRunning: false
   };
 
+  handleStopwatch = () => {
+    this.setState({
+      isRunning: !this.state.isRunning
+    });
+  };
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>
+        <button onClick={this.handleStopwatch}>
           {this.state.isRunning ? 'Stop' : 'Start'}
         </button>
         <button>Reset</button>
