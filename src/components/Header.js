8746978f71d0149ccf7e2e09b5655c2e324ceaab
Gabbie <gabbie.metheny@gmail.com>
2018-10-13 06:22:26 -0700

    refactor to use destructuring for props (variable assignment)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
+  const {players, title} = props;
   return (
     <header>
-      <Stats players={props.players} />
-      <h1>{props.title}</h1>
+      <Stats players={players} />
+      <h1>{title}</h1>
       <Stopwatch />
     </header>
   );
