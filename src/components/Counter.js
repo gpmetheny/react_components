e070cc95a1690bc48c104df66f75f504de4f1324
Gabbie <gabbie.metheny@gmail.com>
2018-10-13 06:28:09 -0700

    refactor to use destructuring for props (function parameters)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Counter = (props) => {
+const Counter = ({index, score, changeScore}) => {
 
   return (
     <div className="counter">
       <button
         className="counter-action decrement"
-        onClick={() => props.changeScore(props.index, -1)}
+        onClick={() => changeScore(index, -1)}
       > - </button>
-      <span className="counter-score">{props.score}</span>
+      <span className="counter-score">{score}</span>
       <button
         className="counter-action increment"
-        onClick={() => props.changeScore(props.index, 1)}
+        onClick={() => changeScore(index, 1)}
       > + </button>
     </div>
   );
