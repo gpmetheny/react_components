1376a744cc2ea3371bf132dac5649cb561d0f6b3
Gabbie <gabbie.metheny@gmail.com>
2018-10-14 06:39:08 -0700

    convert class component to stateless functional component

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,29 +1,27 @@
 import React, {Component} from 'react';
 
-class AddPlayerForm extends Component {
-  playerInput = React.createRef();
+const AddPlayerForm = ({addPlayer}) => {
+  let playerInput = React.createRef();
 
-  handleSubmit = (e) => {
+  let handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addPlayer(this.playerInput.current.value);
+    addPlayer(playerInput.current.value);
     e.currentTarget.reset();
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          ref={this.playerInput}
-          placeholder="Enter a player's name"
-        />
-        <input
-          type="submit"
-          value="Add Player"
-        />
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        ref={playerInput}
+        placeholder="Enter a player's name"
+      />
+      <input
+        type="submit"
+        value="Add Player"
+      />
+    </form>
+  );
+};
 
 export default AddPlayerForm;
