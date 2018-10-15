c22f651de7fe6354baca9a08d99638a8bb783103
Gabbie <gabbie.metheny@gmail.com>
2018-10-15 06:03:07 -0700

    add PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
@@ -10,6 +11,11 @@ const Header = ({players, title}) => {
       <Stopwatch />
     </header>
   );
+};
+
+Header.propTypes = {
+  players: PropTypes.arrayOf(PropTypes.object).isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default Header;
