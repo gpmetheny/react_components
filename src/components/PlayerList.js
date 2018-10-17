de75a913f60d3d4a37c7724c94834ca630ef30c7
Gabbie <gabbie.metheny@gmail.com>
2018-10-17 06:44:15 -0700

    refactor to resemble Guil's code

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Player from './Player';
 
 const PlayerList = ({players, getHighScore, changeScore, removePlayer}) => {
   return (
-    <div>
+    // avoids unnecessary wrapper element, i.e. a div
+    <React.Fragment>
       {players.map((player, index) =>
         <Player
+          // same as: id={player.id} name={player.name} score={player.score}
+          {...player}
           index={index}
           key={player.id.toString()}
-          id={player.id}
-          name={player.name}
-          score={player.score}
           isHighScore={getHighScore() === player.score}
           changeScore={changeScore}
           removePlayer={removePlayer}
         />
       )}
-    </div>
+    </React.Fragment>
   );
+};
+
+PlayerList.propTypes = {
+  players: PropTypes.array.isRequired,
+  getHighScore: PropTypes.func.isRequired,
+  changeScore: PropTypes.func.isRequired,
+  removePlayer: PropTypes.func.isRequired
 };
 
 export default PlayerList;
